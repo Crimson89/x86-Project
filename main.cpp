625e315400b24316d985e7ed8dd044324a5988e7
Julian Saunders <sjulian@pdx.edu>
2018-02-15 15:17:34 -0800

    Just come basic flow stuff

--- main.cpp
+++ main.cpp
@@ -4,7 +4,22 @@
 
 int main()
 {
+  int pc; // Program counter
+  int memlength; // Total length of memory array
 
-  printf("Look we compile!\n");
 
+  //open file
+  //allocate memory for size of file
+  //store size in memlength
+  //read and store data in memory
+  //close file
+
+  for (pc = 0; pc < memlenght; pc = pc+4)
+  {
+    //case statements for instructions go here
+    //there are a lot of functions
+    //we might want to break them down into subgroups
+  }
+
+  return 0;
 }
