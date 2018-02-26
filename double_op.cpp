feb9a264e1d4f1b3b535438ad42c02a29ec422f4
Julian Saunders <sjulian@pdx.edu>
2018-02-25 20:03:29 -0800

    Working on double ops

--- double_op.cpp
+++ double_op.cpp
@@ -3,14 +3,41 @@
 // General
 int MOV(instruction *inst) // Move source to destination (B)
 {
+  inst->dest = (byteMode)? (inst->src&0x00FF):inst->src;
+  inst->N = (inst->src < 0)? 1:0; // Set Negative flag if below zero
+  inst->Z = (inst->src == 0)? 1:0; // Set Zero flag if zero
+  inst->V = 0; // Clear
+  return 0;
 }
 
 int ADD(instruction *inst) // Add source to destination
 {
+  int temp = inst->dest;
+  inst->dest = inst->dest + inst->src; // Do the add
+  inst->N = (inst->dest < 0)? 1:0;
+  inst->Z = (inst->dest == 0)? 1:0;
+
+  // Overflow, i.e. pos + pos = neg
+  inst->V = ((temp > 0 && inst->src > 0 && inst->dest < 0) || (temp < 0 && inst->src < 0 && inst->dest > 0))? 1:0;
+
+  // Set C flag if there was a carry from the MSB
+  // inst->C = ()? 1:0;
+
 }
 
 int SUB(instruction *inst) // Subtract source from destination
 {
+  int temp = inst->dest;
+  inst->dest = inst->dest - inst->src;
+  inst->N = (inst->dest < 0)? 1:0;
+  inst->Z = (inst->dest == 0)? 1:0;
+
+  // Overflow, i.e. pos - neg = neg or neg - pos = pos
+  inst->V = ((temp > 0 && inst->src < 0 && inst->dest < 0) || (temp < 0 && inst->src > 0 && inst->dest > 0))? 1:0;
+
+  // Set C flag if there was a carry from the MSB
+  // inst->C = ()? 1:0;
+
 }
 
 int CMP(instruction *inst) // Compare source to destination (B)
