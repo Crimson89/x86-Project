4feb130577a7d6fc7fee3d19529289abd2e107e5
dmchristiansen <dmc5@pdx.edu>
2018-02-26 23:47:45 -0800

    fixed defines

--- defines.h
+++ defines.h
@@ -17,131 +17,132 @@
 // Opcode values
 
 // SINGLE OPERAND
-
 //TODO I think we need to add a leading 0 to some of these.
 // General
-#define CLR    "005000" // Clear
-#define CLRB   "105000" // Clear (B)
-#define COM    "005100" // 1's Compliment
-#define COMB   "105100" // 1's Compliment (B)
-#define INC    "005200" // Increment
-#define INCB   "105200" // Increment (B)
-#define DEC    "005300" // Decrement
-#define DECB   "105300" // Decrement (B)
-#define NEG    "005400" // 2's Compliment negate
-#define NEGB   "105400" // 2's Compliment negate (B)
-#define NOP    "000240" // No operation
-#define TST    "005700" // Test
-#define TSTB   "105700" // Test (B)
-#define TSTSET // Test, set low bit
-#define WRTLCK // Read/Lock destination, write/unlock R0 #defineo destination
+#define m_CLR    005000 // Clear
+#define m_CLRB   105000 // Clear (B)
+#define m_COM    005100 // 1's Compliment
+#define m_COMB   105100 // 1's Compliment (B)
+#define m_INC    005200 // Increment
+#define m_INCB   105200 // Increment (B)
+#define m_DEC    005300 // Decrement
+#define m_DECB   105300 // Decrement (B)
+#define m_NEG    005400 // 2's Compliment negate
+#define m_NEGB   105400 // 2's Compliment negate (B)
+#define m_NOP    000240 // No operation
+#define m_TST    005700 // Test
+#define m_TSTB   105700 // Test (B)
+#define m_TSTSET // Test, set low bit
+#define m_WRTLCK // Read/Lock destination, write/unlock R0 #defineo destination
 
 // Shift & Rotate
-#define ASR    "006200" // Arithmetic shift right
-#define ASRB   "106200" // Arithmetic shift right (B)
-#define ASL    "006300" // Arithmetic shift left
-#define ASLB   "106300" // Arithmetic shift left (B)
-#define ROR    "006000" // Rotate right
-#define RORB   "106000" // Rotate right (B)
-#define ROL    "006100" // Rotate left
-#define ROLB   "106100" // Rotate left (B)
-#define SWAB   "000300" // Swap bytes
+#define m_ASR    006200 // Arithmetic shift right
+#define m_ASRB   106200 // Arithmetic shift right (B)
+#define m_ASL    006300 // Arithmetic shift left
+#define m_ASLB   106300 // Arithmetic shift left (B)
+#define m_ROR    006000 // Rotate right
+#define m_RORB   106000 // Rotate right (B)
+#define m_ROL    006100 // Rotate left
+#define m_ROLB   106100 // Rotate left (B)
+#define m_SWAB   000300 // Swap bytes
 
 // Multiple Precision
-#define ADC    "005500" // Add carry
-#define ADCB   "105500" // Add carry (B)
-#define SBC    "005600" // Subtract carry
-#define SBCB   "105600" // Subtract carry (B)
-#define SXT    "006700" // Sign extend
+#define m_ADC    005500 // Add carry
+#define m_ADCB   105500 // Add carry (B)
+#define m_SBC    005600 // Subtract carry
+#define m_SBCB   105600 // Subtract carry (B)
+#define m_SXT    006700 // Sign extend
 
 // DOUBLE OPERAND
 
 // General
-#define MOV    "010000" // Move source to destination
-#define MOVB   "110000" // Move source to destination (B)
-#define ADD    "060000" // Add source to destination
-#define SUB    "160000" // Subtract source from destination
-#define CMP    "020000" // Compare source to destination
-#define CMPB   "120000" // Compare source to destination (B)
-#define ASH    "072000" // Shift arithmetically
-#define ASHC   "073000"// Arithmetic shift combined
-#define MUL    "070000" // Multiply
-#define DIV    "071000" // Divide
+#define m_MOV    010000 // Move source to destination
+#define m_MOVB   110000 // Move source to destination (B)
+#define m_ADD    060000 // Add source to destination
+#define m_SUB    160000 // Subtract source from destination
+#define m_CMP    020000 // Compare source to destination
+#define m_CMPB   120000 // Compare source to destination (B)
+#define m_ASH    072000 // Shift arithmetically
+#define m_ASHC   073000// Arithmetic shift combined
+#define m_MUL    070000 // Multiply
+#define m_DIV    071000 // Divide
 
 // Logical
-#define BIT    "030000" // Bit test
-#define BITB   "130000" // Bit test (B)
-#define BIC    "040000" // Bit clear
-#define BICB   "140000" // Bit clear (B)
-#define BIS    "050000" // Bit set
-#define BISB   "150000" // Bit set (B)
-#define XOR    "074000" // Exclusive OR
+#define m_BIT    030000 // Bit test
+#define m_BITB   130000 // Bit test (B)
+#define m_BIC    040000 // Bit clear
+#define m_BICB   140000 // Bit clear (B)
+#define m_BIS    050000 // Bit set
+#define m_BISB   150000 // Bit set (B)
+#define m_XOR    074000 // Exclusive OR
 
 // BRANCH
 
 // Branch
-#define BR     "000400" // Branch unconditional
-#define BNE    "001000" // Branch if not equal (to zero)
-#define BEQ    "001400" // Branch if equal (to zero)
-#define BPL    "100000" // Branch if plus
-#define BMI    "100400" // Branch if minus
-#define BVC    "102000" // Branch if overflow is clear
-#define BVS    "102400" // Branch if overflow is set
-#define BCC    "103000" // Branch if carry is clear
-#define BCS    "103400" // Branch if carry is set
+#define m_BR     000400 // Branch unconditional
+#define m_BNE    001000 // Branch if not equal (to zero)
+#define m_BEQ    001400 // Branch if equal (to zero)
+#define m_BPL    100000 // Branch if plus
+#define m_BMI    100400 // Branch if minus
+#define m_BVC    102000 // Branch if overflow is clear
+#define m_BVS    102400 // Branch if overflow is set
+#define m_BCC    103000 // Branch if carry is clear
+#define m_BCS    103400 // Branch if carry is set
 
 // Signed Conditional Branch
-#define BGE    "002000" // Branch if greater than or equal (to zero)
-#define BLT    "002400" // Branch if less than (zero)
-#define BGT    "003000" // Branch if greater than (zero)
-#define BLE    "003400" // Branch if less than or equal (to zero)
-#define SOB    "077000" // Subtract one and branch (if not = 0)
+#define m_BGE    002000 // Branch if greater than or equal (to zero)
+#define m_BLT    002400 // Branch if less than (zero)
+#define m_BGT    003000 // Branch if greater than (zero)
+#define m_BLE    003400 // Branch if less than or equal (to zero)
+#define m_SOB    077000 // Subtract one and branch (if not = 0)
 
 // Unsigned Conditional Branch
-#define BHI    "101000" // Branch if higher
-#define BLOS   "101400" // Branch if lower or same
-#define BHIS   "103000" // Branch if higher or same (same as BCC)
-#define BLO    "103400" // Branch if lower (same as BCS)
+#define m_BHI    101000 // Branch if higher
+#define m_BLOS   101400 // Branch if lower or same
+#define m_BHIS   103000 // Branch if higher or same (same as BCC)
+#define m_BLO    103400 // Branch if lower (same as BCS)
 
 // JUMP & SUBROUTINE
 
-#define JMP    "000100" // Jump
-#define JSR    "004000" // Jump to subroutine
-#define RTS    "000200" // Return from subroutine
-#define MARK   "006400" // Facilitates stack clean-up procedures
+#define m_JMP    000100 // Jump
+#define m_JSR    004000 // Jump to subroutine
+#define m_RTS    000200 // Return from subroutine
+#define m_MARK   006400 // Facilitates stack clean-up procedures
 
 // TRAPS & INTERRUPTS
 
-#define EMT    "104000" // Emulator trap
-#define TRAP   "104400" // Trap
-#define BPT    "000003" // Breakpoint trap
-#define IOT    "000004" // Input/Output trap
-#define CSM    // Call to supervisor mode
-#define RTI    "000002" // Return from interrupt
-#define RTT    "000002" // Return from trace trap (Same as RTI but supresses the immidialty-following trace trap)
+#define m_EMT    104000 // Emulator trap
+#define m_TRAP   104400 // Trap
+#define m_BPT    000003 // Breakpoint trap
+#define m_IOT    000004 // Input/Output trap
+#define m_CSM    // Call to supervisor mode
+#define m_RTI    000002 // Return from interrupt
+#define m_RTT    000002 // Return from trace trap (Same as RTI but supresses the immidialty-following trace trap)
 
 // MISC
 
-#define HALT   "000000" // Halt
-#define WAIT   "000001" // Wait for interrupt
-#define RESET  "000005" // Reset UNIBUS
-#define MTPD   "106600" // Move to previous data space
-#define MTPI   "006600" // Move to previous instruction space
-#define MFPD   "106500" // Move from previous data space
-#define MFPI   "006500" // Move from previous instruction space
-#define MTPS   "106400" // Move byte to processor status word
-#define MFPS   "106700" // Move byte from processor status word
-#define MFPT   // Move from processor type
+#define m_HALT   000000 // Halt
+
+#define m_WAIT   000001 // Wait for interrupt
+#define m_RESET  000005 // Reset UNIBUS
+#define m_MTPD   106600 // Move to previous data space
+#define m_MTPI   006600 // Move to previous instruction space
+#define m_MFPD   106500 // Move from previous data space
+#define m_MFPI   006500 // Move from previous instruction space
+#define m_MTPS   106400 // Move byte to processor status word
+#define m_MFPS   106700 // Move byte from processor status word
+#define m_MFPT   // Move from processor type
 
 // CONDITION CODES
 
-#define CLC   "000241" // Clear carry
-#define CLV   "000242" // Clear overflow
-#define CLZ   "000244" // Clear zero
-#define CLN   "000250" // Clear negative
-#define CCC   "000257" // Clear all flags
-#define SEC   "000261" // Set carry
-#define SEV   "000262" // Set overflow
-#define SEZ   "000264" // Set zero
-#define SEN   "000270" // Set negative
-#define SCC   "000277" // Set all flags
+#define m_CLC   000241 // Clear carry
+#define m_CLV   000242 // Clear overflow
+#define m_CLZ   000244 // Clear zero
+#define m_CLN   000250 // Clear negative
+#define m_CCC   000257 // Clear all flags
+#define m_SEC   000261 // Set carry
+#define m_SEV   000262 // Set overflow
+#define m_SEZ   000264 // Set zero
+#define m_SEN   000270 // Set negative
+#define m_SCC   000277 // Set all flags
+
--- header.h
+++ header.h
@@ -14,6 +14,31 @@
 
 using namespace std;
 
+//instructionType instructionFamily {singleOperand, doubleOperand}; // (? Probably not necessary) 
+ 
+enum addressingModeSrc {test1}; 
+ 
+enum addressingModeDest {test2}; 
+ 
+typedef struct {  
+  uint16_t opcode;  
+  uint16_t byteMode;  
+  //instructionFamily instructionType ; //Double operand, single operand, conditional jump, shit like that.  
+  //enum instructionFamily; // J, I, R? Not in PDP right?  
+  uint16_t addressingModeSrc;  
+  uint16_t addressingModeDest;  
+  uint16_t reg; // uint16_t? 
+  uint16_t src;  
+  uint16_t dest;  
+  uint16_t offset; 
+  uint16_t registerMode;  
+  bool SC;
+  bool N; // Something better than booleans?  
+  bool Z; 
+  bool V;  
+  bool C;  
+  uint16_t rtsR; // placeholder till I figure this out.
+}instruction;  
 
 // MAIN functions
 int loadData(int argc, char ** argv);
@@ -146,74 +171,49 @@ int SCC(instruction *inst); // Set Condition Codes
 
 // Parsing functions
 
-//instructionType instructionFamily {singleOperand, doubleOperand}; // (? Probably not necessary) 
- 
-enum addressingModeSrc {test1}; 
- 
-enum addressingModeDest {test2}; 
- 
-typedef struct {  
-  uint16_t opcode;  
-  uint16_t byteMode;  
-  //instructionFamily instructionType ; //Double operand, single operand, conditional jump, shit like that.  
-  //enum instructionFamily; // J, I, R? Not in PDP right?  
-  uint16_t addressingModeSrc;  
-  uint16_t addressingModeDest;  
-  uint16_t reg; // uint16_t? 
-  uint16_t src;  
-  uint16_t dest;  
-  uint16_t offset; 
-  uint16_t registerMode;  
-  bool SC;
-  bool N; // Something better than booleans?  
-  bool Z; 
-  bool V;  
-  bool C;  
-  uint16_t rtsR; // placeholder till I figure this out.
-}instruction;  
   
-const unsigned char maskByteMode = 0x8000; 
+const uint16_t maskByteMode = 0x8000; 
  
 // BETTER NAMES MAX TODO double check these masks to see if they're doing what I want them to do.  
-const unsigned char maskRelevantBits = 0x78A0;  
-const unsigned char maskSingleCondBranchCondCheck = 0x7000; // (Maybe 0x7800)  
-const unsigned char maskSingle = 0x0800; // (Maybe 0x7800) 
-const unsigned char maskCondCheck = 0x00E0; // (Maybe 0xFF80) Else conditional branch  
-const unsigned char maskRegSource = 0x7000; // (Maybe 0x8000) Else double operand  
+const uint16_t maskRelevantBits = 0x78A0;  
+const uint16_t maskSingleCondBranchCondCheck = 0x7000; // (Maybe 0x7800)  
+const uint16_t maskSingle = 0x0800; // (Maybe 0x7800) 
+const uint16_t maskCondCheck = 0x00E0; // (Maybe 0xFF80) Else conditional branch  
+const uint16_t maskRegSource = 0x7000; // (Maybe 0x8000) Else double operand  
 
 //TODO change opcode stuff to match Julian's definitions (maybe in octal).
 // Assignment masks
 // Double-operand
-const unsigned char maskDoubleOpcode = 0x7000;
-const unsigned char maskDoubleSourceMode = 0x0E00;
-const unsigned char maskDoubleSource = 0x01C0;
-const unsigned char maskDoubleDestMode = 0x0038;
-const unsigned char maskDoubleDest = 0x0007;
+const uint16_t maskDoubleOpcode = 0x7000;
+const uint16_t maskDoubleSourceMode = 0x0E00;
+const uint16_t maskDoubleSource = 0x01C0;
+const uint16_t maskDoubleDestMode = 0x0038;
+const uint16_t maskDoubleDest = 0x0007;
 
 // Double-operand register
-const unsigned char maskDoubleRegisterOpcode = 0x0700;
-const unsigned char maskDoubleRegisterReg = 0x00C0;
-const unsigned char maskDoubleRegisterSourceDestMode = 0x0038;
-const unsigned char maskDoubleRegisterSourceDest = 0x0007;
+const uint16_t maskDoubleRegisterOpcode = 0x0700;
+const uint16_t maskDoubleRegisterReg = 0x00C0;
+const uint16_t maskDoubleRegisterSourceDestMode = 0x0038;
+const uint16_t maskDoubleRegisterSourceDest = 0x0007;
 
 // Single-operand
-const unsigned char maskSingleOpcode = 0x0730;
-const unsigned char maskSingleMode = 0x0038;
-const unsigned char maskSingleRegister = 0x0007;
+const uint16_t maskSingleOpcode = 0x0730;
+const uint16_t maskSingleMode = 0x0038;
+const uint16_t maskSingleRegister = 0x0007;
 
 // Conditional branch
-const unsigned char maskCondBranchOpcode = 0x0700;
-const unsigned char maskCondBranchOffset = 0x00FF;
+const uint16_t maskCondBranchOpcode = 0x0700;
+const uint16_t maskCondBranchOffset = 0x00FF;
 
 // Conditional check
-const unsigned char maskCondCodeOpcode = 0xFFE0;
-const unsigned char maskCondSC = 0x0010;
-const unsigned char maskCondN = 0x0008;
-const unsigned char maskCondZ = 0x0004;
-const unsigned char maskCondV = 0x0002;
-const unsigned char maskCondC = 0x0001;
+const uint16_t maskCondCodeOpcode = 0xFFE0;
+const uint16_t maskCondSC = 0x0010;
+const uint16_t maskCondN = 0x0008;
+const uint16_t maskCondZ = 0x0004;
+const uint16_t maskCondV = 0x0002;
+const uint16_t maskCondC = 0x0001;
 
 // Byte mask
-const unsigned char maskByteInstruction = 0x8000;
+const uint16_t maskByteInstruction = 0x8000;
 
 int parseInstruction(uint16_t instructionCode, instruction* newInstruction);
