661619ecdddb7e328cc67a7db0c6e287850db9d6
Julian Saunders <sjulian@pdx.edu>
2018-03-07 20:12:46 -0800

    single and double op read/write

--- double_op.cpp
+++ double_op.cpp
@@ -3,61 +3,94 @@
 // General Instructions
 int MOV(instruction *inst) // Move source to destination (B)
 {
-  inst->dest = (byteMode)? (inst->src&0x00FF):inst->src;
-  inst->N = (inst->src < 0)? 1:0; // Set Negative flag if below zero
-  inst->Z = (inst->src == 0)? 1:0; // Set Zero flag if zero
+  if(inst->byteMode)
+  {
+    uint16_t a_src = read_byte(inst->src);
+    write_byte(inst->dest, a_src);
+  }
+  else
+  {
+    uint16_t a_src = read_word(inst->src);
+    write_word(inst->dest, a_src);
+  }
+  uint16_t v_src = get_value(inst->src);
+  inst->N = (v_src < 0)? 1:0; // Set Negative flag if below zero
+  inst->Z = (v_src == 0)? 1:0; // Set Zero flag if zero
   inst->V = 0; // Clear
   return 0;
 }
 
 int ADD(instruction *inst) // Add source to destination
 {
-  uint16_t temp = inst->dest;
-  inst->dest = inst->dest + inst->src; // Do the add
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  // Read values from memory
+  uint16_t v_dest = get_value(0, inst->dest);
+  uint16_t v_src = get_value(0, inst->src);
+  uint16_t temp = v_dest;
 
-  // Overflow, i.e. pos + pos = neg
-  inst->V = ((temp > 0 && inst->src > 0 && inst->dest < 0) || (temp < 0 && inst->src < 0 && inst->dest > 0))? 1:0;
+  // Do the operation and write to memory
+  v_dest += v_src;
+  set_value(v_dest, inst->dest);
+
+  // Set NZVC flags
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
+
+  // Overflow, i.e. pos + pos = neg, neg + neg = pos
+  inst->V = ((temp > 0 && v_src > 0 && v_dest < 0) || (temp < 0 && v_src < 0 && v_dest > 0))? 1:0;
 
   // Set C flag if there was a carry to the MSB
-  inst->C = (inst->dest > 0xFFFF) ? 1:0;
+  inst->C = (v_dest > 0xFFFF) ? 1:0;
   return 0;
 }
 
 int SUB(instruction *inst) // Subtract source from destination
 {
-  uint16_t temp = inst->dest;
-  inst->dest = inst->dest - inst->src;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  uint16_t v_dest = get_value(0, inst->dest);
+  uint16_t v_src = get_value(0, inst->src);
+  uint16_t temp = v_dest;
+
+  v_dest -= v_src;
+  set_value(v_dest, inst->dest);
+
+  // Set NZVC flags
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
 
   // Overflow, i.e. pos - neg = neg or neg - pos = pos
-  inst->V = ((temp > 0 && inst->src < 0 && inst->dest < 0) || (temp < 0 && inst->src > 0 && inst->dest > 0))? 1:0;
+  inst->V = ((temp > 0 && v_src < 0 && v_dest < 0) || (temp < 0 && v_src > 0 && v_dest > 0))? 1:0;
 
   // Set C flag if there was a carry from the MSB
-  inst->C = (((temp > 0xFFFF) || (inst->src > 0xFFFF)) && (inst->dest <= 0xFFFF))? 1:0;
+  inst->C = (((temp > 0xFFFF) || (v_src > 0xFFFF)) && (v_dest <= 0xFFFF))? 1:0;
   return 0;
 }
 
 int CMP(instruction *inst) // Compare source to destination (B)
 {
-  uint16_t temp = inst->src - inst->dest;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t v_src = get_value(inst->byteMode, inst->src);
+
+  uint16_t temp = v_src - v_dest;
   inst->N = (temp < 0)? 1:0;
   inst->Z = (temp == 0)? 1:0;
 
   // Overflow, i.e. pos - neg = neg or neg - pos = pos
-  inst->V = ((inst->src > 0 && inst->dest < 0 && temp < 0) || (inst->src < 0 && inst->dest > 0 && temp > 0))? 1:0;
+  inst->V = ((v_src > 0 && v_dest < 0 && temp < 0) || (v_src < 0 && v_dest > 0 && temp > 0))? 1:0;
 
   // Set C flag if there was a carry from the MSB
-  inst->C = (((temp > 0xFFFF) || (inst->src > 0xFFFF)) && (inst->dest <= 0xFFFF))? 1:0;
+  if(inst->byteMode)
+    inst->C = (((temp > 0xFF) || (v_src > 0xFF)) && (v_dest <= 0xFF))? 1:0;
+  else
+    inst->C = (((temp > 0xFFFF) || (v_src > 0xFFFF)) && (v_dest <= 0xFFFF))? 1:0;
   return 0;
 }
 
 // Logical
 int BIT(instruction *inst) // Bit test (B)
 {
-  uint16_t temp = inst->dest && inst->src;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t v_src = get_value(inst->byteMode, inst->src);
+
+  uint16_t temp = v_dest && v_src;
   inst->N = (temp < 0)? 1:0;
   inst->Z = (temp == 0)? 1:0;
   inst->V = 0;
@@ -66,18 +99,28 @@ int BIT(instruction *inst) // Bit test (B)
 
 int BIC(instruction *inst) // Bit clear (B)
 {
-  inst->dest = ~(inst->src) && inst->dest;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t v_src = get_value(inst->byteMode, inst->src);
+
+  v_dest = ~(v_src) && v_dest;
+  set_value(v_dest, inst->dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
   inst->V = 0;
   return 0;
 }
 
 int BIS(instruction *inst) // Bit set (B)
 {
-  inst->dest = inst->src || inst->dest;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t v_src = get_value(inst->byteMode, inst->src);
+
+  v_dest = v_src || v_dest;
+  set_value(v_dest, inst->dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
   inst->V = 0;
   return 0;
 }
--- single_op.cpp
+++ single_op.cpp
@@ -3,7 +3,11 @@
 // General
 int CLR(instruction *inst) // Clear (B)
 {
-  inst->dest = 0;
+  uint16_t v_dest = 0;
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
   inst->N = 0;
   inst->Z = 1;
   inst->V = 0;
@@ -13,9 +17,16 @@ int CLR(instruction *inst) // Clear (B)
 
 int COM(instruction *inst) // 1's Compliment (B)
 {
-  inst->dest = ~inst->dest;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  v_dest = ~v_dest;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
   inst->V = 0;
   inst->C = 1;
   return 0;
@@ -23,22 +34,36 @@ int COM(instruction *inst) // 1's Compliment (B)
 
 int INC(instruction *inst) // Increment (B)
 {
-  uint16_t temp = inst->dest;
-  inst->dest++;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
-  inst->V = ((temp == 0xFFFF) && (inst->dest == 0x0000))? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t temp = v_dest;
+  v_dest++;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
+  inst->V = ((temp == 0xFFFF) && (v_dest == 0x0000))? 1:0;
   inst->C = 0;
   return 0;
 }
 
 int DEC(instruction *inst) // Decrement (B)
 {
-  uint16_t temp = inst->dest;
-  inst->dest--;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
-  inst->V = ((temp == 0x0000) && (inst->dest == 0xFFFF))? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t temp = v_dest;
+  v_dest--;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
+  inst->V = ((temp == 0x0000) && (v_dest == 0xFFFF))? 1:0;
   inst->C = 0;
   return 0;
 }
@@ -49,8 +74,9 @@ int NEG(instruction *inst) // 2's Compliment negate (B)
 
 int TST(instruction *inst) // Test (B)
 {
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
   inst->V = 0;
   inst->C = 0;
   return 0;
@@ -59,57 +85,92 @@ int TST(instruction *inst) // Test (B)
 // Shift & Rotate
 int ASR(instruction *inst) // Arithmetic shift right (B)
 {
-  inst->C = inst->dest & 1; // C = old LSB
-  inst->dest >>= 1;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  inst->C = v_dest & 1; // C = old LSB
+  v_dest >>= 1;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
   inst->V = inst->N ^ inst->C;
   return 0;
 }
 
 int ASL(instruction *inst) // Arithmetic shift left (B)
 {
-  inst->C = inst->dest >> (sizeof(inst->dest)*8-1) & 1; // C = old MSB
-  inst->dest <<= 1;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  inst->C = v_dest >> (sizeof(v_dest)*8-1) & 1; // C = old MSB
+  v_dest <<= 1;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
   inst->V = inst->N ^ inst->C;
   return 0;
 }
 
 int ROR(instruction *inst) // Rotate right (B)
 {
-  uint16_t temp2 = inst->dest;
-  uint16_t temp = 0x0001 & inst->dest;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t temp2 = v_dest;
+  uint16_t temp = 0x0001 & v_dest;
   temp <<= 15;
-  (inst->dest >>= 1) | temp;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
-  inst->C = (inst->dest > temp2)? 1:0;
+  (v_dest >>= 1) | temp;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
+  inst->C = (v_dest > temp2)? 1:0;
   inst->V = inst->N ^ inst->C;
   return 0;
 }
 
 int ROL(instruction *inst) // Rotate left (B)
 {
-  uint16_t temp2 = inst->dest;
-  uint16_t temp = 0x8000 & inst->dest;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t temp2 = v_dest;
+  uint16_t temp = 0x8000 & v_dest;
   temp >>= 15;
-  (inst->dest <<= 1) | temp;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
-  inst->C = (inst->dest < temp2)? 1:0;
+  (v_dest <<= 1) | temp;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
+  inst->C = (v_dest < temp2)? 1:0;
   inst->V = inst->N ^ inst->C;
   return 0;
 }
 
 int SWAB(instruction *inst) // Swap bytes
 {
-  uint16_t temp = 0x00FF & inst->dest;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t temp = 0x00FF & v_dest;
   temp <<= 8;
-  (inst->dest >>= 8) | (temp);
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
+  (v_dest >>= 8) | (temp);
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
   inst->C = 0;
   inst->V = 0;
   return 0;
@@ -118,21 +179,35 @@ int SWAB(instruction *inst) // Swap bytes
 // Multiple Precision
 int ADC(instruction *inst) // Add carry (B)
 {
-  uint16_t temp = inst->dest;
-  inst->dest += inst->C;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
-  inst->C = ((temp == 0xFFFF) && (inst->dest == 0x0000))? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t temp = v_dest;
+  v_dest += inst->C;
+
+   if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
+  inst->C = ((temp == 0xFFFF) && (v_dest == 0x0000))? 1:0;
   return 0;
 }
 
 int SBC(instruction *inst) // Subtract carry (B)
 {
-  uint16_t temp = inst->dest;
-  inst->dest -= inst->C;
-  inst->N = (inst->dest < 0)? 1:0;
-  inst->Z = (inst->dest == 0)? 1:0;
-  inst->C = ((temp == 0x0000) && (inst->dest == 0xFFFF))? 1:0;
+  uint16_t v_dest = get_value(inst->byteMode, inst->dest);
+  uint16_t temp = v_dest;
+  v_dest -= inst->C;
+
+  if(inst->byteMode)
+    write_byte(inst->addressMode, inst->dest, v_dest);
+  else
+    write_word(inst-a>addressMode, inst->dest, v_dest);
+
+  inst->N = (v_dest < 0)? 1:0;
+  inst->Z = (v_dest == 0)? 1:0;
+  inst->C = ((temp == 0x0000) && (v_dest == 0xFFFF))? 1:0;
   return 0;
 }
 
