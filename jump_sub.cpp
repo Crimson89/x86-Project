d8d8af448eabebf39b8a982e9a98202463c167f7
Daniel Christiansen <dmc5@pdx.edu>
2018-03-09 00:23:29 -0800

    started jump things

--- jump_sub.cpp
+++ jump_sub.cpp
@@ -2,6 +2,21 @@
 
 int JMP(instruction *inst) // Jump
 {
+  uint16_t destination;
+
+  // If dest addressing mode = 0, "illegal instruction"
+  if(inst->addressingModeReg == 0)
+    return -1;
+
+  destination = get_value(inst->addressingModeRed, inst->reg);
+   
+  // If value is an odd address, "boundar error trap condition"
+  if(destination & 0x0001)
+    return -1;
+
+  PC = destination;
+
+  return 0;
 }
 
 int JSR(instruction *inst) // Jump to subroutine
