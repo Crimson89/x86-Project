da45ed22019a78c539847c0e00e60f759228f560
Daniel Christiansen <dmc5@pdx.edu>
2018-03-09 21:46:03 -0800

    added JST & RST instructions

--- jump_sub.cpp
+++ jump_sub.cpp
@@ -1,24 +1,75 @@
 #include "header.h"
 
-int JMP(instruction *inst) // Jump
+// Jump & subroutine instructions
+
+// Jump
+int JMP(instruction *inst)
 {
   uint16_t destination;
 
   // If dest addressing mode = 0, "illegal instruction"
-  if(inst->addressingModeReg == 0)
+  if(inst->addressingModeReg == 0) {
+    cout << oct << "Illegal Instruction @ " << PC-2 
+      << ".  Program control cannot be transferred to a register.\n";
     return -1;
+  }
 
-  destination = get_value(inst->addressingModeRed, inst->reg);
+  destination = get_value(inst->addressingModeReg, inst->reg);
    
   // If value is an odd address, "boundar error trap condition"
-  if(destination & 0x0001)
+  if(destination & 0x0001) {
+    cout << oct << "Boundary error @ " << PC-2
+      << ". Misaligned jump destination: " << destination << ".\n";
     return -1;
+  }
 
   PC = destination;
 
   return 0;
 }
 
-int JSR(instruction *inst) // Jump to subroutine
+// Jump to subroutine
+int JSR(instruction *inst)
 {
+  // (tmp) <- (dest) (dest might be SP, get this value before pushing reg)
+  // -(SP) <- reg
+  // reg <- PC
+  // PC <- (tmp)
+  
+  uint16_t tmp;
+  
+  // tmp <- dest
+  dst_tmp = get_value(inst->addressingModeDest, inst->dest);
+  
+  // -(SP) <- reg
+  reg_tmp = get_value(00, inst->reg);
+  SP -= 2;
+  write_word(01, SP, reg_tmp, TRUE);
+
+  // reg <- PC
+  write_word(00, inst->reg, PC, FALSE);
+
+  // PC <- tmp
+  write_word(00, PC, dst_tmp, FALSE);
+
+  return 0;
 }
+
+// Return from subroutine
+int RST(instruction *inst)
+{
+  uint16_t tmp;
+
+  // PC <- reg
+  tmp = get_value(00, inst->reg);
+  write_word(00, PC, tmp, FALSE);
+
+  // reg <- (SP)+
+  tmp = get_value(01, SP);
+  write_word(00, inst->reg, tmp, FALSE);
+  SP += 2;
+
+  return 0;
+}
+
+
